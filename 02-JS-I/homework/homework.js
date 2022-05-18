5fc608211aaddea38ab0ac8415351f817ade9903
FranPicci432 <franciscopiccinetti5@gmail.com>
2022-05-18 16:56:38 -0300

    modifique la booleana

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Holaa";
 const nuevoNum = 3 ;
 
 // Crea una variable booleana:
-const nuevoBool = var 3 = true
+const nuevoBool = true ;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5 ;
