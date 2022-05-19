35aa5a9bf31e76fcf0da10d1094c60ce6f55f550
FranPicci432 <franciscopiccinetti5@gmail.com>
2022-05-19 03:23:06 -0300

    cambios en el commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -130,8 +130,15 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  if (num % 3 === 1, 3, 5, 7) {
+    return true
+  }
+  else {
+    return false
+  }
 }
+
+
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
